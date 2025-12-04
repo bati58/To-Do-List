a074b032d99322d03acb645beb8514481cf722f8
bati jano <batijano58@gmail.com>
2025-12-04 17:43:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-//variables
-
 const addTask = document.getElementById("add-task");
 const taskContainer = document.getElementById("task-container");
 const inputTask = document.getElementById("input-task");
 
 //event listener for add button
-
 addTask.addEventListener("click", function () {
   let task = document.createElement("div");
   task.classList.add("task");
 
-  /*let li = document.createElement('li');
-  li.innerText = '${inputTask.Value}';
-  task.appendChild(li);*/
   let li = document.createElement("li");
   li.innerText = inputTask.value;
   task.appendChild(li);
 
+  // Check button
   let checkButton = document.createElement("button");
   checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
   checkButton.classList.add("checkTask");
   task.appendChild(checkButton);
 
+  // Edit button (NEW)
+  let editButton = document.createElement("button");
+  editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  editButton.classList.add("editTask");
+  task.appendChild(editButton);
+
+  // Delete button
   let deleteButton = document.createElement("button");
   deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
   deleteButton.classList.add("deleteTask");
@@ -32,13 +34,38 @@ addTask.addEventListener("click", function () {
   } else {
     taskContainer.appendChild(task);
   }
-  inputTask.value = " ";
+  inputTask.value = "";
+
+  // Check task
   checkButton.addEventListener("click", function () {
-    checkButton.parentElement.style.textDecoration = "line-through";
+    li.style.textDecoration =
+      li.style.textDecoration === "line-through" ? "none" : "line-through";
   });
 
-  deleteButton.addEventListener("click", function (e) {
-    let target = e.target;
-    target.parentElement.parentElement.remove();
+  // Delete task
+  deleteButton.addEventListener("click", function () {
+    task.remove();
+  });
+
+  // Edit task (NEW)
+  editButton.addEventListener("click", function () {
+    if (editButton.innerHTML.includes("pen")) {
+      // change to input mode
+      let editInput = document.createElement("input");
+      editInput.type = "text";
+      editInput.value = li.innerText;
+      task.replaceChild(editInput, li);
+
+      editButton.innerHTML = '<i class="fa-solid fa-save"></i>';
+      li = editInput;
+    } else {
+      // save edited task
+      let newLi = document.createElement("li");
+      newLi.innerText = li.value;
+      task.replaceChild(newLi, li);
+
+      editButton.innerHTML = '<i class="fa-solid fa-pen"></i>';
+      li = newLi;
+    }
   });
 });
